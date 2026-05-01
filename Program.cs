26d3c2673cc5c665fc97e389cb7b9e20a058b1bb
Kanan <aygunyagublu@outlook.com>
2026-05-01 23:42:28 +0200

    Add Swagger UI for API documentation

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@ using ProductService.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title       = "Product Service API",
+        Version     = "v1",
+        Description = "ASP.NET Core 8 ile yazilmish mehsul idareetme API-si"
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ProductRepository>();
@@ -17,6 +28,14 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
 }
+
+/* Swagger həmişə aktiv */
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Service v1");
+    c.RoutePrefix = string.Empty; /* ana səhifə Swagger olsun */
+});
 
 app.UseHttpsRedirection();
 app.MapControllers();
